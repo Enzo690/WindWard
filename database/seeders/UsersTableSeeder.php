a2411192170430d9e27b69897518e6e77124064b
Enzo690 <ethibert@edenschool.fr>
2021-03-18 16:50:54 +0100

    add user migration

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -5,15 +5,17 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class UsersTableSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-        User::factory(10)->create();
+        User::withoutEvents(function () {
+            User::factory()->create();
+        });
     }
 }
